9a83722ab3975608290ee61cec45f5d6121c29ba
Kseniia <kseniia.pomortseva@gmail.com>
2021-09-30 13:04:54 +0500

    lesson4

--- index.php
+++ index.php
@@ -1,102 +1,114 @@
 <?php
   //Задача №1
-  //Дан массив [33, 42, -7, -81, 11, -2, 21, 0, 5, 67]. По заданному пользователем индексу (должно быть числом) проверить существует ли данный элемент.
+  //С помощью цикла (без использования % и implode) вывести на экран только десятки (10, 20, 30 ...) из диапазона от 10 до 200 включительно. Числа разделить запятыми, после последнего числа должна быть точка.
 
-  //a) если нет, добавить элемент с введенным индексом и присвоить ему значение 0
-
-  //b) если существует, то проверить, если его значение отрицательное, перезаписать его как положительное. проверить, если оно положительное, вывести следующий за ним элемент
-
-  //c) если значение равно нулю, то переместить данный элемент в конец массива
-
-  //Вывести введенный индекс и итоговый массив на экран.
-
-/*  $arr = [33, 42, -7, -81, 11, -2, 21, 0, 5, 67];
-
-  $index = readline('Insert an index ');
-
-  if ($index >= 0 && is_numeric($index)) {
-
-    if (isset($arr[$index])) {
-      switch (true) {
-        case ($arr[$index] < 0):
-          $arr[$index] = abs($arr[$index]);
-          echo "Элемент массива под индексом {$index} преобразован из отрицательного числа в положительное, записан как {$arr[$index]}" . PHP_EOL;
-          break;
-        case ($arr[$index] == 0):
-          echo "Под индексом {$index} находился элемент со значением 0, он перемещен в конец массива." . PHP_EOL;
-          array_push($arr, 0);
-          unset($arr[$index]);
-          break;
-        default:
-          echo "Под индексом {$index} в массиве находится число {$arr[$index]}. Следующий элемент {$arr[$index+1]}" . PHP_EOL;
-          break;
-      }
-    } else {
-      $arr[$index] = 0;
-      echo "Под индексом {$index} элемента в массиве не было. Добавлен элемент под введеным индексом, ему присвоено значение 0" . PHP_EOL;
-    }
-  } else {
-    echo "Введите целое число, которое равно или больше 0" . PHP_EOL;
-  }
-
-  var_dump($arr);
-*/
+  /* for ($i=10; $i <= 200; $i += 10) { 
+    if ($i == 200) echo "$i." . PHP_EOL; else echo "$i, ";
+  }*/
 
   //Задача №2
+  //Сформировать массив из 15 случайных целых чисел из интервала от -100 до 100 включительно. Вывести его на экран.
+  //a) посчитать и вывести сумму всех нечетных элементов массива
+  //b) найти минимальный элемент в массиве и вывести его на экран
 
-  //Дан массив:
-  /*
-  [
-    'monday' => 'понедельник',
-    'tuesday' => 'вторник',
-    'wednesday' => 'среда',
-    'thursday' => 'четверг',
-    'friday' => 'пятница',
-    'saturday' => 'суббота'
-    'sunday' => 'воскресенье'
-  ]*/
+  /*$arr = [];
+    $sum = 0;
+    $min = 100;
 
-/*
-  $arr = [
-    'monday' => 'понедельник',
-    'tuesday' => 'вторник',
-    'wednesday' => 'среда',
-    'thursday' => 'четверг',
-    'friday' => 'пятница',
-    'saturday' => 'суббота',
-    'sunday' => 'воскресенье'
-  ];
+    for ($i=0; $i <= 15 ; $i++) { 
+      $arr[] = rand(-100, 100);
+      if ($i % 2 != 0) {
+        $sum += $arr[$i];
+      }
+      if ($arr[$i] < $min) {
+        $min = $arr[$i];
+      }
+    }
+
+    var_dump($arr);
+    echo 'Сумма всех элементов массива: ' . $sum . PHP_EOL;
+    echo 'Минимальный элемент массива: ' . $min . PHP_EOL;
+  */
   
-  //a) сформировать 2 новых индексированных массива с днями на англ. и рус. языках
 
-  $arr_en = array_keys($arr);
-  $arr_ru = array_values($arr);
+  //Задача №3
+  //Вывести с помощью циклов "треугольник"
 
-  //b) вывести оба массива в виде строк с разделителем ,
-
-  echo 'Дни недели на английском языке: ' . implode(', ', $arr_en) . PHP_EOL;
-  echo 'Дни недели на русском языке: ' . implode(', ', $arr_ru) . PHP_EOL;
-*/
-
-//Задание №3
-
-/*
-  $text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel tempus erat. In sollicitudin nisl nisi, in cursus erat pulvinar et. In congue eleifend accumsan. Nam dictum, nibh a justo iaculis, at hendrerit dui condimentum. Nulla et malesuada elit. Etiam eu dolor et nulla lobortis lacinia malesuada quis lacus. Aliquam nec nibh porta, vehicula justo id, sodales eros. Nulla facilisi. Nulla quis dui volutpat, mattis massa ut, interdum nisl.';
-
-  //a) выяснить кол-во слов в тексте, если оно больше 50, вывести предупреждение о превышении данного лимита
-  $arr_text_words = explode(' ', $text);
-
-  if (count($arr_text_words) > 50) {
-    echo "Текст превышает лимит в 50 слов. Количество слов " . count($arr_text_words) . PHP_EOL;
+  /*for ($i = 1; $i < 10; $i++) { 
+    for ($n = 0; $n < $i; $n++) { 
+      echo $i;
+    }
+    echo PHP_EOL;
   }
 
-  //b) выяснить кол-во предложений в данном тексте и вывести на экран
-  $arr_text_sent = explode('.', $text);
-  echo "Количество предложений в тексте " . (count($arr_text_sent) - 1) . PHP_EOL;
+  for ($i = 9; $i > 0; $i--) { 
+    for ($n = $i; $n > 0; $n--) { 
+      echo $i;
+    }
+    echo PHP_EOL;
+  }*/
+  
 
-  //c) получить 5е предложение (Nam dictum ...), оно должно начинаться с маленькой буквы. удалить из него все точки, запятые и пробел в начале. пробелы между словами заменить на дефисы - и сформировать из этого предложения ссылку вида http://site.ru/news/(предложение). вывесит ссылку на экран.
+  //Задача №4
+  //Есть массив с днями недели на нескольких языках. Запрашивать повторный ввод (в пунктах a и b), если пользователь ввел неправильные данные (элемент не существует или неверный индекс).
 
-  $sentence = str_replace(' ', '-', str_replace(',', '', lcfirst(trim($arr_text_sent[4]))));
-  $link = 'http://site.ru/news/' . $sentence;
-  echo $link . PHP_EOL;
+  //a) по заданному языку вывести все дни недели в обратном порядке
+
+  //b) с помощью цикла по заданному номеру вывести день недели на каждом языке
+
+  //с) вывести на экран все элементы массива, языки в виде ненумерованного списка (<ul>), а дни недели в виде нумерованных подсписков (<ol>)
+
+
+$arr = [
+  'ru' => ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+  'en' => ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+  'de' => ['montag', 'dienstag', 'mittwoch', 'donnerstag', 'freitag', 'samstag', 'sonntag'],
+  'fr' => ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'],
+  'it' => ['lunedì', 'martedì', 'mercoledì', 'giovedì', 'venerdì', 'sabato', 'domenica'],
+  'es' => ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'],
+];
+
+
+/*  $ask_lang = true;
+  $ask_day = true;
+
+  while($ask_lang){
+
+    $lang = readline('Language in two symbols (ru/en/de/fr/it/es): ');
+
+    if (array_key_exists($lang, $arr)) {
+      for ($index=count($arr[$lang]) - 1; $index >= 0 ; $index--) { 
+        echo $arr[$lang][$index] . ' ';
+      }
+      echo PHP_EOL;
+
+      //Заправшиваем день недели
+      while($ask_day){
+
+        $day = readline('Insert day (0-6): ');
+    
+        if ($day > 0 && $day < 7) {
+          echo 'День недели: ' . $arr[$lang][$day] . PHP_EOL;
+          $ask_day = false;//Выход из цикла при верном вводе языка
+        } else {
+          echo 'Вы ввели неверный день недели. Повторите попытку.' . PHP_EOL;//Продолжаем спрашивать день
+        }
+      }
+      $ask_lang = false;//Выход из цикла при верном вводе дня
+    } else {
+      echo 'Введите язык, согласно предложенной рекоммендации.' . PHP_EOL;//Продолжаем спрашивать язык
+    }
+  }
 */
+
+echo "<ul>" . PHP_EOL;
+  foreach ($arr as $lang => $days) {
+    echo "\t<li>$lang" . PHP_EOL;
+    echo "\t\t<ol>" . PHP_EOL;
+    foreach ($days as $day) {
+     echo "\t\t\t<li>$day</li>" . PHP_EOL;
+    }
+    echo "\t\t</ol>" . PHP_EOL;
+    echo "\t</li>" . PHP_EOL;
+  }
+echo "</ul>" . PHP_EOL;
